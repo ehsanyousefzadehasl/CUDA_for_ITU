f80b992ad57fa1615a133c1a4f1e6620167de65b
Ehsan Yousefzadeh-Asl-Miandoab <ehyo@itu.dk>
2024-11-17 14:04:22 +0100

    example task showing how to run tasks on the university's cluster

---
+++ connectHPC/test_cuda.cu
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cuda_runtime.h>
+
+// kernel (function) to be executed on the GPU
+__global__ void helloWorld() {
+        printf("Hello from thread %d from block %d\n", threadIdx.x, blockIdx.x);
+}
+
+int main() {
+        // launch the GPU kernel
+        helloWorld<<<10, 10>>>();
+
+        cudaDeviceSynchronize();
+
+        return 0;
+}
